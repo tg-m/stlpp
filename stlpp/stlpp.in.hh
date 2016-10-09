0acb0e0a318c00f45ba4fa45724df3f91b693f88
TGMarkiewicz <tomasz.g.markiewicz@gmail.com>
2016-10-09 12:33:01 +0200

    Refactor naming

--- stlpp/stlpp.in.hh
+++ stlpp/stlpp.in.hh
@@ -38,6 +38,12 @@ template<typename Container_t> inline auto end(Container_t& c) -> decltype(std::
 template<typename Container_t> inline auto end(Container_t const& c) -> decltype(std::end(c)) {
     return std::end(c);
 }
+#else
+template<typename Container_t> inline typename Container_t::iterator begin(Container_t& c) {return c.begin();}
+template<typename Container_t> inline typename Container_t::iterator begin(Container_t const& c) {return c.begin();}
+template<typename Container_t> inline typename Container_t::iterator end(Container_t& c) {return c.end();}
+template<typename Container_t> inline typename Container_t::iterator end(Container_t const& c) {return c.end();}
+#endif
 template<typename Container_t> inline typename Container_t::iterator rbegin(Container_t& c) {
     return c.rbegin();
 }
@@ -50,101 +56,98 @@ template<typename Container_t> inline typename Container_t::iterator rend(Contai
 template<typename Container_t> inline typename Container_t::iterator rend(Container_t const& c) {
     return c.rend();
 }
-#else
-template<typename Container_t> inline typename Container_t::iterator begin(Container_t& c) {return c.begin();}
-template<typename Container_t> inline typename Container_t::iterator begin(Container_t const& c) {return c.begin();}
-template<typename Container_t> inline typename Container_t::iterator end(Container_t& c) {return c.end();}
-template<typename Container_t> inline typename Container_t::iterator end(Container_t const& c) {return c.end();}
-#endif
-
 }
 /* namespace stdpp */
 
 /*
- * if container will be get as sth.next(), then next will be called twice
+ * NOTE: call_b_e stands for call_begin_end
+ * Names of algorithms that search in the reverse order start with r, e.g., all_of vs rall_of
  */
-#define call_it_it(dir, function, container)\
+#define call_b_e(dir, function, container)\
 std::function(STLPP_namespace::dir##begin(container), STLPP_namespace::dir##end(container))
 
-#define call_it_it_pred(dir, function, container, func)\
+#define call_b_e_it(dir, function, container, it)\
+std::function(STLPP_namespace::dir##begin(container), STLPP_namespace::dir##end(container), it)
+
+#define call_b_e_pred(dir, function, container, func)\
 std::function(STLPP_namespace::begin(container), STLPP_namespace::end(container), pred)
 
-#define call_it_it_compare(dir, function, container, compare)\
+#define call_b_e_compare(dir, function, container, compare)\
 std::function(STLPP_namespace::begin(container), STLPP_namespace::end(container), compare)
 
-#define call_it_it_val(dir, function, container, val)\
+#define call_b_e_val(dir, function, container, val)\
 std::function(STLPP_namespace::begin(container), STLPP_namespace::end(container), val)
 
-#define call_it_it_val_compare(dir, function, container, val, compare)\
+#define call_b_e_val_compare(dir, function, container, val, compare)\
 std::function(STLPP_namespace::begin(container), STLPP_namespace::end(container), val, compare)
 
-//#define GEN_it_it_general(function, dir)\
+//#define GEN_b_e_general(function, dir)\
 //template<typename Container_t>\
 //inline auto dir##function(Container_t& c) -> decltype(std::function(STLPP_namespace::dir##begin(c), STLPP_namespace::dir##end(c))) {\
 //return std::function(STLPP_namespace::dir##begin(c), STLPP_namespace::dir##end(c));\
 //}
 
-#define GEN_general_it_it(Const, direction, function)\
-template<typename Container_t> inline auto direction##function(Container_t Const& c) -> decltype(call_it_it(direction, function, c)) { return call_it_it(direction, function, c); }
-#define GEN_it_it(function)\
-GEN_general_it_it(     ,  , function);CAR_RET;\
-GEN_general_it_it(const,  , function);CAR_RET;\
-GEN_general_it_it(     , r, function);CAR_RET;\
-GEN_general_it_it(const, r, function)
+#define GEN_general_b_e(Const, direction, function)\
+template<typename Container_t> inline auto direction##function(Container_t Const& c) -> decltype(call_b_e(direction, function, c)) { return call_b_e(direction, function, c); }
+#define GEN_b_e(function)\
+GEN_general_b_e(     ,  , function);CAR_RET;\
+GEN_general_b_e(const,  , function);CAR_RET;\
+GEN_general_b_e(     , r, function);CAR_RET;\
+GEN_general_b_e(const, r, function)
 
-#define GEN_general_it_it_pred(Const, direction, function)\
+#define GEN_general_b_e_pred(Const, direction, function)\
 template<typename Container_t, typename Predicate_t>\
-inline auto direction##function(Container_t Const& c, Predicate_t pred) -> decltype(call_it_it_pred(direction, function, c, pred)) {\
-    return call_it_it_pred(direction, function, c, pred);\
+inline auto direction##function(Container_t Const& c, Predicate_t pred) -> decltype(call_b_e_pred(direction, function, c, pred)) {\
+    return call_b_e_pred(direction, function, c, pred);\
 }
-#define GEN_it_it_pred(function)\
-GEN_general_it_it_pred(     ,  , function);CAR_RET;\
-GEN_general_it_it_pred(const,  , function);CAR_RET;\
-GEN_general_it_it_pred(     , r, function);CAR_RET;\
-GEN_general_it_it_pred(const, r, function)
+#define GEN_b_e_pred(function)\
+GEN_general_b_e_pred(     ,  , function);CAR_RET;\
+GEN_general_b_e_pred(const,  , function);CAR_RET;\
+GEN_general_b_e_pred(     , r, function);CAR_RET;\
+GEN_general_b_e_pred(const, r, function)
 
-#define GEN_it_it_and_it_it_pred(function)\
-GEN_it_it(function);CAR_RET;\
-GEN_it_it_pred(function);CAR_RET
+#define GEN_b_e_and_b_e_pred(function)\
+GEN_b_e(function);CAR_RET;\
+GEN_b_e_pred(function);CAR_RET
 
-#define GEN_general_it_it_val(Const, direction, function)\
+#define GEN_general_b_e_val(Const, direction, function)\
 template<typename Container_t, typename T>\
-inline auto direction##function(Container_t Const& c, T const/*This is always const!*/& val) -> decltype(call_it_it_val(direction, function, c, val)) {\
-return call_it_it_val(direction, function, c, val);\
+inline auto direction##function(Container_t Const& c, T const/*This is always const!*/& val) -> decltype(call_b_e_val(direction, function, c, val)) {\
+return call_b_e_val(direction, function, c, val);\
 }
-#define GEN_it_it_val(function)\
-GEN_general_it_it_val(     ,  , function);CAR_RET;\
-GEN_general_it_it_val(const,  , function);CAR_RET;\
-GEN_general_it_it_val(     , r, function);CAR_RET;\
-GEN_general_it_it_val(const, r, function)
+#define GEN_b_e_val(function)\
+GEN_general_b_e_val(     ,  , function);CAR_RET;\
+GEN_general_b_e_val(const,  , function);CAR_RET;\
+GEN_general_b_e_val(     , r, function);CAR_RET;\
+GEN_general_b_e_val(const, r, function)
 
-#define GEN_general_it_it_val_compare(Const, direction, function)\
+#define GEN_general_b_e_val_compare(Const, direction, function)\
 template<typename Container_t, typename T, typename Compare_t>\
-inline auto direction##function(Container_t Const& c, T const/*This is always const!*/& val, Compare_t compare) -> decltype(call_it_it_val_compare(direction, function, c, val, compare)) {\
-return call_it_it_val_compare(direction, function, c, val, compare);\
+inline auto direction##function(Container_t Const& c, T const/*This is always const!*/& val, Compare_t compare) -> decltype(call_b_e_val_compare(direction, function, c, val, compare)) {\
+return call_b_e_val_compare(direction, function, c, val, compare);\
 }
-#define GEN_it_it_val_compare(function)\
-GEN_general_it_it_val_compare(     ,  , function);CAR_RET;\
-GEN_general_it_it_val_compare(const,  , function);CAR_RET;\
-GEN_general_it_it_val_compare(     , r, function);CAR_RET;\
-GEN_general_it_it_val_compare(const, r, function)
+#define GEN_b_e_val_compare(function)\
+GEN_general_b_e_val_compare(     ,  , function);CAR_RET;\
+GEN_general_b_e_val_compare(const,  , function);CAR_RET;\
+GEN_general_b_e_val_compare(     , r, function);CAR_RET;\
+GEN_general_b_e_val_compare(const, r, function)
 
-#define GEN_it_it_val_and_it_it_val_compare(function)\
-GEN_it_it_val(function);CAR_RET;\
-GEN_it_it_val_compare(function)
+#define GEN_b_e_val_and_b_e_val_compare(function)\
+GEN_b_e_val(function);CAR_RET;\
+GEN_b_e_val_compare(function)
 
 namespace STLPP_namespace {
 #define r r
-//GEN_it_it_general(adjacent_find, r);
-//GEN_it_it_general(adjacent_find,);
-GEN_it_it_and_it_it_pred(adjacent_find);
-GEN_it_it_and_it_it_pred(max_element);
-GEN_it_it_val_and_it_it_val_compare(binary_search);
+//GEN_b_e_general(adjacent_find, r);
+//GEN_b_e_general(adjacent_find,);
+GEN_b_e_and_b_e_pred(adjacent_find);
+GEN_b_e_and_b_e_pred(max_element);
+GEN_b_e_val_and_b_e_val_compare(binary_search);
 
 #if __cplusplus >= 201103L
 
-GEN_it_it_pred(any_of);
-GEN_it_it_pred(all_of);
+GEN_b_e_pred(any_of);
+GEN_b_e_pred(all_of);
 
 #endif /* __clpusplus */
 
