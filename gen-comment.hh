78788ab1855f046ad508956531e2d4fd621dca04
TGMarkiewicz <tomasz.g.markiewicz@gmail.com>
2017-02-02 20:33:30 +0100

    Fix grammar error

--- gen-comment.hh
+++ gen-comment.hh
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
- * THIS FILE WAS GENERATED AUTOMATICALLY -- DO NOT MODIFY IT BY MANUALLY!
+ * THIS FILE WAS GENERATED AUTOMATICALLY -- DO NOT MODIFY IT MANUALLY!
  *
  *
  */
